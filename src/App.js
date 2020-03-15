6d71befe456c6b10eaa5ac8e06996c52dfb62f74
opinesc <omarpinera@gmail.com>
2020-03-15 12:31:59 +0100

    Estilos globales

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { useContext} from 'react'
+import React, { useContext, Fragment } from 'react'
 
+import { GlobalStyle } from './styles/GlobalStyles'
 import {Context } from './Context'
 
 export const App =() => {
     const { isAuth } = useContext(Context)
 
     return (
+      <Fragment>
+        <GlobalStyle />
         <h4>Hola OAPE!!!</h4>
+      </Fragment>
     )
 }
